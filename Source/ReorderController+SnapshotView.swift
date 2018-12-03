bf2eb5eebe75f81e32d717e42892faf466221efe
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2018-12-03 12:31:56 +0100

    reloadRows(fade) -> reloadRows(none)

--- Source/ReorderController+SnapshotView.swift
+++ Source/ReorderController+SnapshotView.swift
@@ -28,7 +28,7 @@ extension ReorderController {
         guard let tableView = tableView, let superview = tableView.superview else { return }
         
         removeSnapshotView()
-        tableView.reloadRows(at: [indexPath], with: .fade)
+        tableView.reloadRows(at: [indexPath], with: .none)
         
         guard let cell = tableView.cellForRow(at: indexPath) else { return }
         let cellFrame = tableView.convert(cell.frame, to: superview)
